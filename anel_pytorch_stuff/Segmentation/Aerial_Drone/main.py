caff545d2e34557c5396773a3d235c6a23590a4a
AnelMusic <anelmusic13@gmail.com>
2022-12-10 22:34:57 +0100

    Init commit

--- anel_pytorch_stuff/Segmentation/Aerial_Drone/main.py
+++ anel_pytorch_stuff/Segmentation/Aerial_Drone/main.py
@@ -1,25 +1,29 @@
-import torch
-from model import get_model, train_model
-from dataset import get_dataloaders
 from glob import glob
+
+import torch
+
 from config import config
+from dataset import get_dataloaders
+from model import get_model, train_model
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
-
 def main():
 
-    train_loader, test_loader = get_dataloaders(config["img_path"], 
-                                                config["mask_path"], 
-                                                config["label_file_path"], 
-                                                config["batchsize"],
-                                                config["target_height"],
-                                                config["target_width"])
+    train_loader, test_loader = get_dataloaders(
+        config["img_path"],
+        config["mask_path"],
+        config["label_file_path"],
+        config["batchsize"],
+        config["target_height"],
+        config["target_width"],
+    )
     model = get_model(config["num_clases"])
-    
+
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
     model = train_model(model, train_loader, test_loader, config, optimizer, device)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- anel_pytorch_stuff/Segmentation/Aerial_Drone/model.py
+++ anel_pytorch_stuff/Segmentation/Aerial_Drone/model.py
@@ -1,46 +1,49 @@
 # Create Model:
-from torchvision.models.segmentation import deeplabv3_resnet101, DeepLabV3_ResNet101_Weights
 import torch
 from torch import nn
+from torchvision.models.segmentation import (
+    DeepLabV3_ResNet101_Weights,
+    deeplabv3_resnet101,
+)
+
 
 def get_model(num_classes):
     weights = DeepLabV3_ResNet101_Weights.COCO_WITH_VOC_LABELS_V1
     model = deeplabv3_resnet101(weights, progress=True)
     # Same as before just change out_channels
     model.classifier[4] = nn.Conv2d(model.classifier[-1].in_channels, num_classes, 1, 1)
-    
+
     for param in model.parameters():
         param.requires_grad = True
-    
+
     return model
 
 
 def train_model(model, train_dataloader, test_dataloader, config, optimizer, device):
     epochs = config["epochs"]
-    criterion = config["criterion"] 
+    criterion = config["criterion"]
 
-    
     model.to(device)
     print("Using Device: ", device)
 
     for epoch in range(epochs):
         # training
-        print(f'Current epoch: {epoch+1}')
+        print(f"Current epoch: {epoch+1}")
 
         vall_loss_list = []
         train_loss_list = []
-        
+
         for batch_i, data in enumerate(train_dataloader):
             model.train()
 
-            x,y = data[0].to(device), data[1].to(device) # x = bs, c, height, widhth
-            
-            pred_mask = model(x)  
-            pred_mask = pred_mask['out'] # model creates dict out and aux we need out 
-            
-            bs,channels, height, width = y.shape
-            #print("y before = ", y.shape) # we expect bs, imheight, imwidth but  we got bs, 1, imheight, imwidth
-            y = y.view(bs,height, width)
+            x, y = data[0].to(device), data[1].to(device)  # x = bs, c, height, widhth
+
+            pred_mask = model(x)
+            pred_mask = pred_mask["out"]  # model creates dict out and aux we need out
+
+            bs, channels, height, width = y.shape
+            # print("y before = ", y.shape) # we expect bs, imheight, imwidth but  we got bs, 1, imheight, imwidth
+            y = y.view(bs, height, width)
             loss = criterion(pred_mask, y)
 
             optimizer.zero_grad()
@@ -48,20 +51,20 @@ def train_model(model, train_dataloader, test_dataloader, config, optimizer, dev
             train_loss_list.append(loss.item())
 
             optimizer.step()
-            
+
         # validation
         model.eval()
         for batch_i, data in enumerate(test_dataloader):
-            x,y = data[0].to(device), data[1].to(device)
-            bs,channels, height, width = y.shape
-            y = y.view(bs,height, width)
+            x, y = data[0].to(device), data[1].to(device)
+            bs, channels, height, width = y.shape
+            y = y.view(bs, height, width)
 
-            with torch.no_grad():    
-                pred_mask = model(x)  
-                pred_mask = pred_mask['out']
+            with torch.no_grad():
+                pred_mask = model(x)
+                pred_mask = pred_mask["out"]
             val_loss = criterion(pred_mask, y)
             vall_loss_list.append(val_loss.item())
-            
-        print("Train_loss: ", sum(train_loss_list)/len(train_loss_list))
-        print("Val_loss: ", sum(vall_loss_list)/len(vall_loss_list))
-    return model    
+
+        print("Train_loss: ", sum(train_loss_list) / len(train_loss_list))
+        print("Val_loss: ", sum(vall_loss_list) / len(vall_loss_list))
+    return model
